9b56ee244206421483c7a0c7fe24be38161d854c
winartop <winartop01@gmail.com>
2019-11-25 14:03:26 +0700

    example use Aritmetic by winarto

---
+++ BELAJAR JAVA/PROGRAM/Latihan_Program_DariJeni/Dasar2PemrogramanJava/src/dasar2pemrogramanjava/AritmatikaDemo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dasar2pemrogramanjava;
+
+/**
+ *
+ * @author o
+ */
+public class AritmatikaDemo {
+    
+    public static void main(String[] args){
+        
+        //declaration value variable include in program
+        int i=37;
+        int j=42;
+        double x=27.475;
+        double y=7.22;
+        
+        
+        System.out.println("Variable value :");
+        System.out.println("    i ="+i);
+        System.out.println("    j ="+j);
+        System.out.println("    x ="+x);
+        System.out.println("    y ="+y);
+        
+        
+    }
+}
