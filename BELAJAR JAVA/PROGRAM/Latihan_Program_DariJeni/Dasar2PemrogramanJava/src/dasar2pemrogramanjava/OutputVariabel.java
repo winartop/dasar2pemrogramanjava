4f4d8d074e617e7b5e9e0945dcc4140f2e2b7cbf
winartop <winartop01@gmail.com>
2019-11-25 13:56:24 +0700

    latihan output variabel by winarto

---
+++ BELAJAR JAVA/PROGRAM/Latihan_Program_DariJeni/Dasar2PemrogramanJava/src/dasar2pemrogramanjava/OutputVariabel.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dasar2pemrogramanjava;
+
+/**
+ *
+ * @author o
+ */
+public class OutputVariabel {
+    
+    public static void main(String[] args){
+        int value =10;
+        char x;
+        x='A';
+        
+        System.out.println("value");
+        System.out.println("The value of x = "+ x);
+    }
+}
